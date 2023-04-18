27b89bcc2625f222e6dd2111ddb182964e65bb7b
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2023-04-18 12:57:11 +0200

    Created and connected to the MongoDB database, installed nodemon

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
@@ -6,6 +8,19 @@ app.get('/', (req, res) =>{
     res.send('Hello World!');
 })
 
+
+app.get('/blogs', (req, res) =>{
+    res.send('Hello from blogs');
+})
+
+
+
 app.listen(3500, ()=>{
     console.log('listening on port 3500');
 })
+
+mongoose.connect
+('mongodb+srv://admin:123456arbnor@cluster0.xr1y3vr.mongodb.net/?retryWrites=true&w=majority')
+.then(() =>{
+    console.log('MongoDB database connected successfully');
+}).catch(err=> console.log(err))
